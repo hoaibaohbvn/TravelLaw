7c695fe59b8f1143bcac469cdd3c69529f2b6d72
hoaibaohbvn <domaihoaibao@gmail.com>
2023-09-26 11:02:00 +0700

    Initial commit

---
+++ src/main/java/duan/travellaw/TravelLawApplication.java
@@ -0,0 +1,13 @@
+package duan.travellaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelLawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelLawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/duan/travellaw/TravelLawApplicationTests.java
@@ -0,0 +1,13 @@
+package duan.travellaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelLawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
